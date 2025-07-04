17f238f2c391c10b6776bdf04c008a34b476be25
Jeel Pansuriya <jeelishwa@gmail.com>
2025-07-05 03:21:39 +0530

    support unknown number of comma-separated inputs in add()

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -24,7 +24,14 @@ public class StringCalculatorTest {
     @Test
     public void testTwoNumberReturnsSum() {
         StringCalculator calculator = new StringCalculator();
-        assertEquals(9, calculator.add("3,6"));
+        assertEquals(9, calculator.add("4,5"));
+    }
+
+    @Test
+    public void testMultipleNumbersReturnsSum() {
+        assertEquals(10, new StringCalculator().add("1,2,3,4"));
+        assertEquals(55, new StringCalculator().add("1,2,3,4,5,6,7,8,9,10"));
+        assertEquals(60, new StringCalculator().add("10,20,30"));
     }
 
 }
