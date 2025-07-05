1dafd6a5301951cde28274409554343fa42bda52
Jeel Pansuriya <jeelishwa@gmail.com>
2025-07-05 11:25:04 +0530

    support custom delimiters using //[delimiter]\n[Numbers] syntax

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,11 +5,17 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+        String delimiter = "[,\n]";
 
-        String[] parts = input.split("[,\n]");
+        if(input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf('\n');
+            delimiter = input.substring(2, delimiterEndIndex);
+            input = input.substring(delimiterEndIndex + 1);
+        }
+        String[] parts = input.split(delimiter);
         int sum = 0;
         for (String part : parts) {
-            sum += Integer.parseInt(part.trim());
+            sum += Integer.parseInt(part);
         }
 
         return sum;
