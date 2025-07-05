daa2673b69d99466e8124e64454e696c2f34fc35
Jeel Pansuriya <jeelishwa@gmail.com>
2025-07-05 11:49:06 +0530

    throw exception when input contains negative numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
@@ -8,19 +10,28 @@ public class StringCalculator {
             return 0;
         }
         String delimiter = "[,\n]";
-
         if(input.startsWith("//")) {
             int delimiterEndIndex = input.indexOf('\n');
             String customDelimiter = input.substring(2, delimiterEndIndex);
             delimiter = Pattern.quote(customDelimiter);
             input = input.substring(delimiterEndIndex + 1);
         }
+
         String[] parts = input.split(delimiter);
         int sum = 0;
+        List<Integer> NegativeNumbers = new ArrayList<>();
         for (String part : parts) {
-            sum += Integer.parseInt(part);
+            int number = Integer.parseInt(part);
+            if(number < 0) {
+                NegativeNumbers.add(number);
+            }
+            else {
+                sum += number;
+            }
         }
-
+        if(!NegativeNumbers.isEmpty()) {
+            throw new IllegalArgumentException( "negative numbers not allowed: " + NegativeNumbers.toString().replaceAll("[\\[\\]]", ""));
+        }
         return sum;
     }
 }
